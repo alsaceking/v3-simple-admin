8a290ea3802829d5063109ec5844e237a5fe2c75
jacob <358819135@qq.com>
2022-05-13 15:23:00 +0800

    modify vite.config file publicPath

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/v3-simple-admin/' : '/',
   css: {
     preprocessorOptions: {
       less: {
